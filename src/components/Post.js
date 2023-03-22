ffb4e849e2fe7e639e57d8fa4cd405f40767349a
StuartCowley <voidstalker@live.co.uk>
2023-03-22 20:21:44 +0000

    check post component works, complete tasks

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-const Post = (props) => {
+const Post = ({ postData }) => {
+  const { title, author, isPublished, date, body, tags } = postData;
+
   return (
     <div className="post">
       <div className="post-heading">
-        {
-          // TODO: Use data from props to populate title, author, date
-          //   published, body and tags, validate props
-        }
-        <h2>"title"</h2>
-        {
-          // TODO: show post body if isPublished, or 'Coming soon!' if not
-        }
+        <h2>{title}</h2>
+        {isPublished ? body : "Coming soon!"}
       </div>
-      <div className="post-author">Author: "author"</div>
-      <div className="post-date">Published: "date"</div>
+      <div className="post-author">Author: {author}</div>
+      <div className="post-date">Published: {date}</div>
       <h3>Tags:</h3>
-        {
-          // TODO: Create list of all all provided tags
-        }
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
     </div>
   );
 };
